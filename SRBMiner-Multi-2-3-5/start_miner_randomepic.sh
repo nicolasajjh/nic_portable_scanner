eaee210aa8e16ec947d9578a91e4c5905b9869bc
Nicolasajjh <nicolasajjh@gmail.com>
2023-10-17 16:34:01 +0700

    yes

--- SRBMiner-Multi-2-3-5/start_miner_randomepic.sh
+++
@@ -1,4 +0,0 @@
-echo "Worker name : "
-read worker_name
-echo ./SRBMiner-MULTI --multi-algorithm-job-mode 1 --disable-gpu --algorithm randomepic --pool us.epicmine.io:3333 --wallet nicolasajjh2.$worker_name --password RichNicels1312m=pool --keepalive true
-./SRBMiner-MULTI --multi-algorithm-job-mode 1 --disable-gpu --algorithm randomepic --pool us.epicmine.io:3333 --wallet nicolasajjh2.$worker_name --password RichNicels1312m=pool --keepalive true
